97dce47cb178bcb70da68bb58e91847f16cb3b37
mkhan221223 <96902707+mkhan221223@users.noreply.github.com>
2025-09-03 04:17:10 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ support-hive-my-app-software-engineering-laboratory/src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-semibold text-green-700">About Page</h1>
+      <p>This is your new About page.</p>
+    </main>
+  );
+}
